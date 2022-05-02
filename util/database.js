659987e60bd1858b3c44f75e2e2a29cdf51ce025
vetriswa <vetriswa@gmail.com>
2022-05-02 12:33:29 -0700

    modify

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'tanvika@123'
+const sequelize = new Sequelize('node-complete', 'root', 'tanvika@123', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
